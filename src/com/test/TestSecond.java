fb93a4a8429f9e040859caca076319d147610606
H P <H P@DESKTOP-ODBGCTD>
2022-03-22 13:58:52 +0530

    Done commit

--- src/com/test/TestSecond.java
+++ src/com/test/TestSecond.java
@@ -6,6 +6,8 @@ public class TestSecond {
 		
 		System.out.println("done");
 
+		int a;
 	}
 
 }
+ 
---
+++ src/com/test/TestThri.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class TestThri {
+public static void main(String[] args) {
+	
+	System.err.println("over");
+}
+}
